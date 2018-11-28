f017988c849a49bc5ae15e7e996175bca53b71ed
Zoey007 <msguiyuzhao@gmail.com>
2018-11-27 20:26:07 -0800

    updated button style

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -2,8 +2,9 @@ import React from "react";
 import "../../styles/Jumbotron.css";
 
 const Jumbotron = ({ children }) =>
-  <div className="jumbotron text-center">
-    {children}
-  </div>;
+    <div className="jumbotron text-center container-fluid">
+      {children}
+    </div>
+  ;
 
 export default Jumbotron;
