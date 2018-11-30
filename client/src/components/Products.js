5bec9ff95e44d6ad7202dce5dde76d802e6dd2ef
Zoey007 <msguiyuzhao@gmail.com>
2018-11-29 23:47:34 -0800

    update form style

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
-import DeleteBtn from "./DeleteBtn";		
+import DeleteBtn from "./DeleteBtn";
 import { Input, FormBtn } from "./Form";
 import { Col, Row, Container } from "./Grid";
 import Jumbotron from "./Jumbotron";
 import API from "../utils/API";
 // import { Link } from "react-router-dom";
 import "./Form/DataForm.css";
-import PubSub from 'pubsub-js';
-import { TableContainerProduct, TableRow} from "./Table";
+import PubSub from "pubsub-js";
+import { TableContainerProduct, TableRow } from "./Table";
 import "../styles/Table.css";
 
 class Products extends Component {
   state = {
-    products: [],
+    products: []
   };
 
   handleInputChange = event => {
@@ -22,125 +22,165 @@ class Products extends Component {
     });
   };
 
-handleFormSubmit = event => {
-  event.preventDefault();
-  console.log("Handling new product.. Products.js");
-  API.postProducts({
-    product_name: this.state.product_name,
-    product_description: this.state.product_description,
-    product_quantity: this.state.product_quantity,
-    repRepId: localStorage.getItem('rep_id')
-  })
-  // .then(res => console.log("you have entered your product..Products.js"))
-    .then(res => {
-      console.log('postProducts..Products.js ', res);
-      this.loadProducts();
+  handleFormSubmit = event => {
+    event.preventDefault();
+    console.log("Handling new product.. Products.js");
+    API.postProducts({
+      product_name: this.state.product_name,
+      product_description: this.state.product_description,
+      product_quantity: this.state.product_quantity,
+      repRepId: localStorage.getItem("rep_id")
     })
-  .catch(err => console.log (err));
+      // .then(res => console.log("you have entered your product..Products.js"))
+      .then(res => {
+        console.log("postProducts..Products.js ", res);
+        this.loadProducts();
+      })
+      .catch(err => console.log(err));
 
-  //Clear form data after submit
-  this.setState({
-    product_name: "",
-    product_description: "",
-    product_quantity: "",
-  });
-};
+    //Clear form data after submit
+    this.setState({
+      product_name: "",
+      product_description: "",
+      product_quantity: ""
+    });
+  };
 
-componentWillMount() {
-  this.token = PubSub.subscribe('UPDATE_LIST', this.loadProducts.bind(this));
-}
-
-componentDidMount() {
-  this.loadProducts();
-  PubSub.publish('UPDATE_LIST', this.token)
+  componentWillMount() {
+    this.token = PubSub.subscribe("UPDATE_LIST", this.loadProducts.bind(this));
   }
 
-componentWillUnmount() {
-  PubSub.unsubscribe(this.token);
-}
+  componentDidMount() {
+    this.loadProducts();
+    PubSub.publish("UPDATE_LIST", this.token);
+  }
 
-loadProducts = () => {
-  API.getProducts({
-    repRepId: localStorage.getItem('rep_id')
-  })
-    .then(res => {
-      console.log('response to load products..Products.js');
-      console.log(res);
-      if (res.data) {
-        this.setState({
-          products: res.data
-        })
-      }
+  componentWillUnmount() {
+    PubSub.unsubscribe(this.token);
+  }
+
+  loadProducts = () => {
+    API.getProducts({
+      repRepId: localStorage.getItem("rep_id")
     })
-    .catch(err => {
-      console.log(err)
-    })
-};
+      .then(res => {
+        console.log("response to load products..Products.js");
+        console.log(res);
+        if (res.data) {
+          this.setState({
+            products: res.data
+          });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   addClassToEvenRow = () => {
-    var rows = document.getElementByClassName('table').getElementsByTagName('tr');
+    var rows = document
+      .getElementByClassName("table")
+      .getElementsByTagName("tr");
     for (var x = 0; x < rows.length; x++) {
-      rows[x].className = (x % 2 === 0) ? 'even' : 'odd';
+      rows[x].className = x % 2 === 0 ? "even" : "odd";
     }
   };
 
+  render() {
+    return (
+      <div>
+        {/* Row #1 */}
+        <Jumbotron>
+          <h1 className="heading">Products</h1>
+          <p className="sub-heading">View Product and Inventory Details</p>
+        </Jumbotron>
+        <Row>
+          {/* Product Form */}
+          <Col size="xs-12">
+            <center>
+              <form
+                className="form-form-horizontal DataForm"
+                style={{ display: "block", "text-align": "left" }}
+              >
+                <h2>Add A New Product</h2>
 
-render() {
-return (
-<div>
-  {/* Row #1 */}
-  <Jumbotron>
-    <h1 className="heading">Products</h1>
-    <p className="sub-heading">View Product and Inventory Details</p>
-  </Jumbotron>
-  <Row>
-    {/* Product Form */}
-    <Col size="md-12 sm-12">
-      <center>
-          <form className="form-form-horizontal DataForm" style={{ display: 'block', 'text-align': 'left'}} >
-            <h2>Add A New Product</h2>
+                <label htmlFor="product_name">Product Name:</label>
+                <Input
+                  type="text"
+                  name="product_name"
+                  id="product_name"
+                  tabIndex="1"
+                  placeholder="Add Product Name"
+                  value={this.state.product_name}
+                  onChange={this.handleInputChange}
+                  required
+                />
 
-            <label htmlFor="product_name">Product Name:</label>
-            <Input type="text" name="product_name" id="product_name" tabIndex="1" placeholder="Add Product Name" value={this.state.product_name} onChange={this.handleInputChange} required></Input>
-            
-            <label htmlFor="product_description">Product Description:</label>
-            <Input type="text" name="product_description" id="product_description" tabIndex="2" placeholder="Add Description" value={this.state.product_description} onChange={this.handleInputChange} required></Input>
-            
-            <label htmlFor="product_quantity">Product Quantity:</label>
-            <Input type="number" name="product_quantity" id="product_quantity" tabIndex="2" placeholder="Add Quantity" value={this.state.product_quantity} onChange={this.handleInputChange} required></Input>
-            <FormBtn type="submit" name="product-submit" id="product-submit" tabIndex="4" className="form-control btn" value="submit_product" onClick={this.handleFormSubmit}>Add Product
-          </FormBtn>
-          </form>
-        </center>
-    </Col>
-    </Row>
+                <label htmlFor="product_description">
+                  Product Description:
+                </label>
+                <Input
+                  type="text"
+                  name="product_description"
+                  id="product_description"
+                  tabIndex="2"
+                  placeholder="Add Description"
+                  value={this.state.product_description}
+                  onChange={this.handleInputChange}
+                  required
+                />
 
-    {/* Product Table */}
-      <Row>
-        <Col size="xs-12">
-          <div className='private text-center table-responsive sales-table'>
-            {this.state.products.length ? (
-              <TableContainerProduct>
-                {this.state.products.map(product => (
-                  <TableRow key={product.product_id}>
-                      <td className="col-md-1">{product.product_id}</td>
-                      <td className="col-md-2">{product.product_name}</td>
-                      <td className="col-md-2">{product.product_quantity}</td>
-                      <td className="col-md-3">{product.product_description}</td>
-                  </TableRow>
-                ))}
-              </TableContainerProduct>
-            ) : (
-              <h3>No Records Available</h3>
+                <label htmlFor="product_quantity">Product Quantity:</label>
+                <Input
+                  type="number"
+                  name="product_quantity"
+                  id="product_quantity"
+                  tabIndex="2"
+                  placeholder="Add Quantity"
+                  value={this.state.product_quantity}
+                  onChange={this.handleInputChange}
+                  required
+                />
+                <FormBtn
+                  type="submit"
+                  name="product-submit"
+                  id="product-submit"
+                  tabIndex="4"
+                  className="form-control btn"
+                  value="submit_product"
+                  onClick={this.handleFormSubmit}
+                >
+                  Add Product
+                </FormBtn>
+              </form>
+            </center>
+          </Col>
+        </Row>
+
+        {/* Product Table */}
+        <Row>
+          <div className="private text-center table-responsive sales-table">
+            <Col size="xs-12">
+              {this.state.products.length ? (
+                <TableContainerProduct>
+                  {this.state.products.map(product => (
+                    <TableRow key={product.product_id}>
+                      <td>{product.product_id}</td>
+                      <td>{product.product_name}</td>
+                      <td>{product.product_quantity}</td>
+                      <td>{product.product_description}</td>
+                    </TableRow>
+                  ))}
+                </TableContainerProduct>
+              ) : (
+                <h3>No Records Available</h3>
               )}
+            </Col>
           </div>
-        </Col>
-      </Row>
-  </div>
-
-)
+        </Row>
+      </div>
+    );
+  }
 }
-}
-
 
 export default Products;
